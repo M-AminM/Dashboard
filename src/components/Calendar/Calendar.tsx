66255eb4c60909e48263aa398baad7bfe99bc1e9
Amin <aminfarid1234@gmail.com>
2022-12-05 12:08:10 +0330

    calendar

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,26 +1,94 @@
-import React from "react";
-import FullCalendar from "@fullcalendar/react"; // must go before plugins
-import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import React, { useState } from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
 
 const Calendar = () => {
-  function renderEventContent(eventInfo: any) {
-    return (
-      <>
-        <b>{eventInfo.timeText}</b>
-        <i>{eventInfo.event.title}</i>
-      </>
-    );
-  }
+  const [currentEvents, setCurrentEvents] = useState<any>([]);
+  const clickHandler = (e: any) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete the event '${e.event.title}'`
+      )
+    ) {
+      e.event.remove();
+    }
+  };
+
+  const handleDateClick = (e: any) => {
+    console.log(e);
+
+    const title = prompt("Please enter a new title for your event ");
+    const calendarApi = e.view.calendar;
+    calendarApi.unselect();
+
+    if (title) {
+      calendarApi.addEvent({
+        id: `${e.dateStr}-${title}`,
+        title,
+        start: e.startStr,
+        end: e.endStr,
+        allDay: e.allDay,
+      });
+    }
+  };
+
   return (
     <section className="px-8 py-6 md:p-2">
-      <FullCalendar
-        plugins={[dayGridPlugin]}
-        initialView="dayGridMonth"
-        events={[
-          { title: "event 1", date: "2022-12-01" },
-          { title: "event 2", date: "2022-12-02" },
-        ]}
-      />
+      <div className="flex gap-6 md:flex-col">
+        <div className="p-4 rounded-lg w-1/4 md:w-full md:p-0">
+          <h2 className="text-base font-semibold pb-2 pl-0.5">Events</h2>
+          <div>
+            {currentEvents.map((event: any) => (
+              <div className="rounded-lg bg-midBlue mb-2 p-2" key={event.id}>
+                <div className="flex gap-2 flex-col">
+                  <h3 className="text-sm">{event.title}</h3>
+                  <p className="text-sm">
+                    {formatDate(event.start, {
+                      year: "numeric",
+                      month: "short",
+                      day: "numeric",
+                    })}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="w-full">
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            eventClick={clickHandler}
+            select={handleDateClick}
+            eventsSet={(events) => setCurrentEvents(events)}
+            initialEvents={[
+              {
+                id: "1",
+                title: "All-day event",
+                date: "2022-12-14",
+              },
+              {
+                id: "2",
+                title: "Timed event",
+                date: "2022-12-28",
+              },
+            ]}
+          />
+        </div>
+      </div>
     </section>
   );
 };
